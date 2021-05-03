d7644401f0c93a4f8f3f581f2bec4f7df97f540d
Jacob Miner <jake@steamclock.com>
2021-05-03 16:10:38 -0700

    Add parcelable version of post and user

--- app/src/main/java/com/example/mvvmexample/model/Post.kt
+++ app/src/main/java/com/example/mvvmexample/model/Post.kt
@@ -1,5 +1,7 @@
 package com.example.mvvmexample.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -8,9 +10,13 @@ import kotlinx.serialization.Serializable
  * Created by jake on 30/04/21, 3:19 PM
  */
 @Serializable
+@Parcelize
 data class Post(
     @SerialName("userId") val userId: Int,
     @SerialName("id") val id: Int,
     @SerialName("title") val title: String,
     @SerialName("body") val body: String
-)
+): Parcelable
+
+@Parcelize
+data class PostList(val posts: List<Post>): Parcelable
--- app/src/main/java/com/example/mvvmexample/model/User.kt
+++ app/src/main/java/com/example/mvvmexample/model/User.kt
@@ -1,5 +1,7 @@
 package com.example.mvvmexample.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -7,7 +9,11 @@ import kotlinx.serialization.Serializable
  * MVVMExample
  * Created by jake on 30/04/21, 3:16 PM
  */
+@Parcelize
 @Serializable
 data class User(
     @SerialName("id") val id: Int,
-    @SerialName("name") val name: String)
+    @SerialName("name") val name: String): Parcelable
+
+@Parcelize
+data class UserList(val users: List<User>): Parcelable
