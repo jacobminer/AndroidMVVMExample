6d1ee86baffcaf62073d9f4bb5043907675967de
Jacob Miner <jake@steamclock.com>
2021-05-03 16:10:47 -0700

    Create PostViewState.kt

---
+++ app/src/main/java/com/example/mvvmexample/ui/main/PostViewState.kt
@@ -0,0 +1,10 @@
+package com.example.mvvmexample.ui.main
+
+/**
+ * MVVMExample
+ * Created by jake on 03/05/21, 3:08 PM
+ */
+data class PostViewState(
+    var id: Int,
+    var text: String,
+    var user: String)
