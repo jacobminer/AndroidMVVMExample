209f6592ce7d81c0efa2da36f2072fad4ba786cc
Jacob Miner <jake@steamclock.com>
2021-05-14 13:31:59 -0700

    Move load state out of UsersRepository
    Also set expectations for error handling

--- app/src/main/java/com/example/mvvmexample/repository/UsersRepository.kt
+++ app/src/main/java/com/example/mvvmexample/repository/UsersRepository.kt
@@ -4,7 +4,6 @@ import com.example.mvvmexample.cache.CacheMode
 import com.example.mvvmexample.cache.CacheService
 import com.example.mvvmexample.model.UserList
 import com.example.mvvmexample.service.UsersService
-import com.example.mvvmexample.ui.LoadState
 import kotlinx.coroutines.flow.*
 
 /**
@@ -15,32 +14,18 @@ class UsersRepository(
     private val usersService: UsersService,
     private val cacheService: CacheService
 ) {
-    private val mutableLoadState = MutableStateFlow<LoadState?>(null)
-
-    val loadState = mutableLoadState.asSharedFlow()
-
     // Returns a one-off flow, which can then be subscribed to.
-    // TODO: 13/05/21: This will cause any object subscribed to mutableLoadState to update their load state.
-        // which may or may not be the behaviour we're generally looking for.
+    // Error handling should be done using `.catch { }` by the subscriber on the flow
     fun fetchUsers(userIds: Set<Int>, cacheMode: CacheMode = CacheMode.CacheAndUpdate) = flow {
-        mutableLoadState.emit(LoadState.Loading)
         val cached = cacheService.readFromCache(UsersKey, cacheMode)
         if (cached != null) {
             emit((cached.result as UserList).users)
-            mutableLoadState.emit(LoadState.Success)
             if (cached.shouldExit) { return@flow }
         }
 
-        try {
-            val users = usersService.fetchUsers().filter { userIds.contains(it.id) }
-            emit(users)
-            mutableLoadState.emit(LoadState.Success)
-            if (users.isNotEmpty()) {
-                cacheService.updateCache(UsersKey, UserList(users))
-            }
-        } catch (e: Exception) {
-            mutableLoadState.emit(LoadState.Error(e))
-        }
+        val users = usersService.fetchUsers().filter { userIds.contains(it.id) }
+        if (users.isNotEmpty()) { cacheService.updateCache(UsersKey, UserList(users)) }
+        emit(users)
     }
 
     companion object {
