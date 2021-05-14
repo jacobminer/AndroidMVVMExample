e22ff28da83ae297f9a8092565279239994dff39
Jacob Miner <jake@steamclock.com>
2021-05-14 12:55:16 -0700

    Add Extension functions for better mutable shared state error handling

---
+++ app/src/main/java/com/example/mvvmexample/extensions/MutableStateFlowExtensions.kt
@@ -0,0 +1,52 @@
+package com.example.mvvmexample.extensions
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.mapNotNull
+
+/**
+ * MVVMExample
+ * Created by jake on 14/05/21, 12:52 PM
+ */
+sealed class ThrowableFlow<out T: Any> {
+    data class Error(val throwable: Throwable): ThrowableFlow<Nothing>()
+    data class Success<out T: Any>(val data: T): ThrowableFlow<T>()
+
+    companion object {
+        fun error(throwable: Throwable) = Error(throwable)
+        fun <T: Any> success(data: T) = Success(data)
+        suspend fun <T: Any> from(function: suspend () -> T, onError: (suspend (Throwable) -> Unit)?): ThrowableFlow<T> {
+            return try {
+                success(function.invoke())
+            } catch(e: Exception) {
+                onError?.invoke(e)
+                error(e)
+            }
+        }
+    }
+}
+
+@Suppress("FunctionName")
+fun <T : Any> MutableStateThrowableFlow(value: T): MutableStateFlow<ThrowableFlow<T>> = MutableStateFlow(ThrowableFlow.success(value))
+fun <T: Any> StateFlow<ThrowableFlow<T>>.catchError(error: (Throwable) -> Unit): Flow<T> {
+    return mapNotNull { result ->
+        when (result) {
+            is ThrowableFlow.Error -> {
+                error(result.throwable)
+                null
+            }
+            is ThrowableFlow.Success -> result.data
+        }
+    }
+}
+
+suspend fun <T: Any> MutableStateFlow<ThrowableFlow<T>>.emitThrowable(throwable: Throwable) {
+    emit(ThrowableFlow.error(throwable))
+}
+suspend fun <T: Any> MutableStateFlow<ThrowableFlow<T>>.emitData(data: T) {
+    emit(ThrowableFlow.success(data))
+}
+suspend fun <T: Any> MutableStateFlow<ThrowableFlow<T>>.emitFrom(onError: (suspend (Throwable) -> Unit)? = null, function: suspend () -> T) {
+    emit(ThrowableFlow.from(function, onError))
+}
